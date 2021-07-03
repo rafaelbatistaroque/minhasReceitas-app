2b22268a6eb75fcfdc0362340fe961fea6a3bfed
Rafael Batista <maranata_rafa@hotmail.com>
2021-07-03 01:14:32 -0400

    LIBRARY: add expo api to fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import { Fraunces_300Light, Fraunces_400Regular, Fraunces_700Bold } from "@expo-google-fonts/fraunces";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 import React from 'react';
-import { View } from 'react-native';
 import { Categorias } from "./src/screens";
 
 export default function App() {
+    const [fontsCarregadas] = useFonts({
+        Fraunces_300Light,
+        Fraunces_400Regular,
+        Fraunces_700Bold
+    });
+
+    if(fontsCarregadas === false)
+        return <AppLoading />;
+
     return (
-        <View>
-            <Categorias />
-            <StatusBar style="auto" />
-        </View>
+        <Categorias />
     );
 }
