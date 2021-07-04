528493fbf5a6a8871ade316e56a1d0966b153f1e
Rafael Batista <maranata_rafa@hotmail.com>
2021-07-04 03:18:51 -0400

    FEATURES:  add simple structure od RECEITAS screen

--- src/screens/Receitas/index.tsx
+++ src/screens/Receitas/index.tsx
@@ -1,11 +1,28 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
 import React from "react";
 import { Text, View } from "react-native";
+import { CATEGORIAS } from "../../data/dummy-data";
+import { CategoriaProps } from "../../types";
 import { styles } from "./styles";
 
+type Props = {
+    categoriaId: string;
+};
+
 export const Receitas: React.FC = () => {
+    const { params } = useRoute();
+    const { categoriaId } = params as Props;
+    const { setOptions } = useNavigation();
+
+    const [receita, setReceita] = React.useState<CategoriaProps>(CATEGORIAS.find(c => c.id === categoriaId) as CategoriaProps);
+
+    React.useEffect(() => {
+        setOptions({ title: `Receitas ${receita.titulo}` });
+    }, [receita]);
+
     return (
         <View style={styles.container}>
-            <Text>Receitas</Text>
+            <Text>{receita.titulo}</Text>
         </View>
     );
 };
