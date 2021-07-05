c2c2b66b4bf75618f29a6b8a5ebcc900c8de4068
Rafael Batista <maranata_rafa@hotmail.com>
2021-07-04 23:01:21 -0400

    REFACT: change colors app

--- src/data/categorias-dummy.ts
+++ src/data/categorias-dummy.ts
@@ -1,14 +1,14 @@
 import { Categoria } from "./../types";
 
 export const CATEGORIAS: Categoria[] = [
-    { id: 'c1', titulo: 'Italian', cor: '#f5428d' },
-    { id: 'c2', titulo: 'Quick & Easy', cor: '#f54242' },
-    { id: 'c3', titulo: 'Hamburgers', cor: '#f5a442' },
-    { id: 'c4', titulo: 'German', cor: '#f5d142' },
-    { id: 'c5', titulo: 'Light & Lovely', cor: '#368dff' },
-    { id: 'c6', titulo: 'Exotic', cor: '#41d95d' },
-    { id: 'c7', titulo: 'Breakfast', cor: '#9eecff' },
-    { id: 'c8', titulo: 'Asian', cor: '#b9ffb0' },
-    { id: 'c9', titulo: 'French', cor: '#ffc7ff' },
-    { id: 'c10', titulo: 'Summer', cor: '#47fced' }
+    { id: "c1", titulo: "Italian", cor: "#5D6129" },
+    { id: "c2", titulo: "Quick & Easy", cor: "#E4C253" },
+    { id: "c3", titulo: "Hamburgers", cor: "#F39E70" },
+    { id: "c4", titulo: "German", cor: "#D33530" },
+    { id: "c5", titulo: "Light & Lovely", cor: "#EBCD80" },
+    { id: "c6", titulo: "Exotic", cor: "#5F6F3E" },
+    { id: "c7", titulo: "Breakfast", cor: "#FBAC2E" },
+    { id: "c8", titulo: "Asian", cor: "#6D7ABB" },
+    { id: "c9", titulo: "French", cor: "#F05644" },
+    { id: "c10", titulo: "Summer", cor: "#EB7934" }
 ];
